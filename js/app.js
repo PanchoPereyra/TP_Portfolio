13efdb6cf9677773b07219dcab94729bc6a57386
Pancho_Pereyra <panchopereyra@gmail.com>
2025-08-14 13:55:51 -0300

    Ultimando detalles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-document.getElementById("menu-toggle").addEventListener("click", function() {
-  document.getElementById("nav-links").classList.toggle("show");
+// document.getElementById("menu-toggle").addEventListener("click", function() {
+//   document.getElementById("nav-links").classList.toggle("show");
+// });
+
+document.querySelector("#scroll-arriba").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 });
+
+
